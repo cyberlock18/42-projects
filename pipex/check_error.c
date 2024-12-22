180516ab325b7fcaba96d5019f83bb1104f3f571
ruben ortiz izquierdo <ruben.ortizizquierdo7@gmail.com>
2024-12-22 16:43:00 +0100

    normalmode

--- pipex/check_error.c
+++ pipex/check_error.c
@@ -6,7 +6,7 @@
 /*   By: ruortiz- <ruortiz-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/17 21:32:53 by ruortiz-          #+#    #+#             */
-/*   Updated: 2024/11/19 20:06:50 by ruortiz-         ###   ########.fr       */
+/*   Updated: 2024/12/22 16:41:50 by ruortiz-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,21 +20,21 @@ void	check_exec_status(pid_t pid, int *status, char *command)
 	if (pid > 0)
 	{
 		waitpid(pid, status, 0);
-		if (WIFEXITED(*status))
+		if ((*status & 0x7F) == 0) 
 		{
-			exit_code = WEXITSTATUS(*status);
+			exit_code = (*status >> 8) & 0xFF; 
 			if (exit_code == 127)
 			{
 				ft_printf("Command not found: %s\n", command);
 			}
 			exit(exit_code);
 		}
-		else if (WIFSIGNALED(*status))
+		else if ((*status & 0x7F) != 0) 
 		{
-			signal_code = WTERMSIG(*status);
-			ft_printf("Command '%s' was terminated by signal %d\n", command,
-				signal_code);
-			exit(128 + signal_code);
+			signal_code = *status & 0x7F; 
+			ft_printf("Command '%s' was terminated by signal %d\n",
+				command, signal_code);
+			exit(128 + signal_code); 
 		}
 	}
 }
