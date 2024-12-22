1b0b6cee50a6a5ebc69223d6361fe4b6e9c52324
ruben ortiz izquierdo <ruben.ortizizquierdo7@gmail.com>
2024-12-22 15:52:58 +0100

    ´try2

--- pushswap/case_smll.c
+++ pushswap/case_smll.c
@@ -6,7 +6,7 @@
 /*   By: ruortiz- <ruortiz-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/18 14:40:13 by ruortiz-          #+#    #+#             */
-/*   Updated: 2024/12/21 21:42:29 by ruortiz-         ###   ########.fr       */
+/*   Updated: 2024/12/22 15:42:44 by ruortiz-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,13 +14,13 @@
 
 static void	sort_three(t_node **stack_a)
 {
-	int second;
-    int	first;
-    int	third;
-	
-first = (*stack_a)->value;
-second = (*stack_a)->next->value;
-    	third = (*stack_a)->next->next->value;
+	int	first;
+	int	second;
+	int	third;
+
+	first = (*stack_a)->value;
+	second = (*stack_a)->next->value;
+	third = (*stack_a)->next->next->value;
 	if (first > second && second < third && first < third)
 		sa(stack_a);
 	else if (first > second && second > third)
@@ -39,11 +39,11 @@ second = (*stack_a)->next->value;
 		rra(stack_a);
 }
 
-// Encuentra el índice mínimo
 static int	find_min_index(t_node *stack)
 {
-	int	min_index = stack->index;
+	int	min_index;
 
+	min_index = stack->index;
 	while (stack)
 	{
 		if (stack->index < min_index)
@@ -53,14 +53,16 @@ static int	find_min_index(t_node *stack)
 	return (min_index);
 }
 
-// Empuja el menor elemento desde stack_a a stack_b
 static void	push_min_to_b(t_node **stack_a, t_node **stack_b)
 {
-	int	min_index = find_min_index(*stack_a);
+	int	min_index;
+	int	position;
 
+	min_index = find_min_index(*stack_a);
+	position = find_position(*stack_a, min_index);
 	while ((*stack_a)->index != min_index)
 	{
-		if ((*stack_a)->position <= stack_size(*stack_a) / 2)
+		if (position <= stack_size(*stack_a) / 2)
 			ra(stack_a);
 		else
 			rra(stack_a);
@@ -68,35 +70,43 @@ static void	push_min_to_b(t_node **stack_a, t_node **stack_b)
 	pb(stack_a, stack_b);
 }
 
-// Ordena una pila pequeña
+static void	push_and_sort(t_node **stack_a, t_node **stack_b, int size)
+{
+	while (size-- > 3)
+	{
+		push_min_to_b(stack_a, stack_b);
+	}
+	sort_three(stack_a);
+	while (*stack_b)
+	{
+		pa(stack_a, stack_b);
+	}
+}
+
 void	sort_small_stack(t_node **stack_a, t_node **stack_b)
 {
-	int	size = stack_size(*stack_a);
+	int	size;
 
+	size = stack_size(*stack_a);
 	if (size == 2)
 		sa(stack_a);
 	else if (size == 3)
 		sort_three(stack_a);
 	else
-	{
-		while (size-- > 3)
-			push_min_to_b(stack_a, stack_b);
-		sort_three(stack_a);
-		while (*stack_b)
-			pa(stack_a, stack_b);
-	}
+		push_and_sort(stack_a, stack_b, size);
 }
-int find_position(t_node *stack, int target_index)
-{
-    int position;
 
-    position = 0;
-    while (stack)
-    {
-        if (stack->index == target_index)
-            return (position);
-        position++;
-        stack = stack->next;
-    }
-    return (-1); // Si no encuentra el índice
+int	find_position(t_node *stack, int target_index)
+{
+	int	position;
+
+	position = 0;
+	while (stack)
+	{
+		if (stack->index == target_index)
+			return (position);
+		position++;
+		stack = stack->next;
+	}
+	return (-1);
 }
